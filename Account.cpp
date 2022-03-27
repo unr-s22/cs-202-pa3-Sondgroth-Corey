59a4868f71e94be02ed246a6ff0324fe66e858d8
CS-202.1001- Corey Sondgroth <89423132+Sondgroth-Corey@users.noreply.github.com>
2022-03-26 17:39:59 -0700

    progect 3

---
+++ Account.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Account.h"
+
+Account::Account (Money cashInAccount){
+    initialBalance = cashInAccount;
+}
+
+//I have to go do something but i think you just need to add function with those vectors i made in Account.h so just make one  Deposit and one make withdrawls 
--- Account.h
+++ Account.h
@@ -1,14 +1,27 @@
 #include <iostream>
 #include "Money.h"
+#include "vector"
 
 class Account{
-	double initialBalance;
+	Money initialBalance;
+
+//These are the vector for depositing and withdrawing money
+	std::vector <Money> withdraws;
+	std::vector <Money> deposits;
+
+	int totalDeposits = 0, totalWidthdraws = 0;
+
+
 	
 	public:
-	 Account(Money){
-		initialBalance = 300.23;
-	}
+	 Account(Money);
+
+//You didnt need to set account balance casue its calling from my Money class
 
 	  void makeDeposit(Money);
 	  void makeWithdrawals(Money);
+	  
+
+	  friend std::ostream operator <<(std::ostream& stream, const Account& account);
 };
+
