792e79df6c405b8ec536f9d7e9147f1d4f5b8044
CS-202.1001- Corey Sondgroth <89423132+Sondgroth-Corey@users.noreply.github.com>
2022-03-29 13:16:29 -0700

    project3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Account.h"
 #include <string>
 using namespace std;
+
 int main()
 {
    Account account(Money(300,23));
@@ -21,45 +22,54 @@ cout << account << endl;
 
 
 //might have gotten carried away with the funtion test, but it was fun!
+//testing the + - == < > and !=
 Money money1(1100,10);
 Money money2(100,10);
 Money money3(100,10);
 Money money4(1000,50);
+Money totalInBothAccounts;
+
+totalInBothAccounts = money1 + money2;
+cout <<"Account 1 and Account 2 added together: " <<  totalInBothAccounts << endl;
+totalInBothAccounts = money4 - money3;
+cout <<"Account 4  subratcted by Account 3: "<<totalInBothAccounts << endl;
+
+
 if(money1 > money2){
-    cout <<  "Money1 is bigger then Money2"<<endl;
+    cout <<  "\nMoney1 is bigger then Money2"<<endl;
 }
 else if (money1 < money2){
-    cout <<  "Money1 is smaller then Money2"<<endl;
+    cout <<  "\nMoney1 is smaller then Money2"<<endl;
 }
 else{
-    cout << "Money1 is the same as Money2"<<endl;
+    cout << "\nMoney1 is the same as Money2"<<endl;
 }
 if(money2 > money3){
-    cout << "Money2 is bigger then Money3"<<endl;
+    cout << "\nMoney2 is bigger then Money3"<<endl;
 }
 else if(money2 < money3){
-    cout << "Money2 is less then Money3"<<endl;
+    cout << "\nMoney2 is less then Money3"<<endl;
 }
 else{
-    cout << "Money3 and Money3 are the same"<<endl;
+    cout << "\nMoney3 and Money3 are the same"<<endl;
 }
 if(money4 > money1 && money4 > money2 && money4 > money3){
-    cout << "Money4 has the most money";
+    cout << "\nMoney4 has the most money";
 }
 else if(money3 > money4 && money3 > money2 && money3 > money1){
-    cout << "Money3 has the most money"<< endl;
+    cout << "\nMoney3 has the most money"<< endl;
 }
 else if(money2 > money4 && money2 > money3 && money2 > money1){
-    cout << "Money2 has the most money"<< endl;
+    cout << "\nMoney2 has the most money"<< endl;
 }
 else{
-    cout << "Money1 has the most money"<<endl;
+    cout << "\nMoney1 has the most money"<<endl;
 }
 if(money1 != money2 && money1 != money3 && money1 != money4 && money2 != money3 && money2 != money4 && money3 != money4){
-    cout << "None of them are equal"<< endl;
+    cout << "\nNone of them are equal"<< endl;
 }
 else {
-    cout << "One of the accounts are equal" << endl;
+    cout << "\nOne of the accounts are equal" << endl;
 }
 
 
